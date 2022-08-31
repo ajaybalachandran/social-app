438951e9b84f5bd493d7c05893f0493bbda9a985
ajay <ajaybalachandran01@gmail.com>
2022-08-31 13:35:09 +0530

    serializer for  user Authentication

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from api.models import Posts
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class PostSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
@@ -7,3 +8,9 @@ class PostSerializer(serializers.ModelSerializer):
         model = Posts
         exclude = ("date", )
         # fields=["title","description","image","user"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email", "username", "password"]
